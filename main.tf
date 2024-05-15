4e2e9a07ef2731b45d045ab2a4fb88a016c9a1ea
sandeep <munagalasandeepreddy@gamail.com>
2024-05-15 17:50:26 +0200

    learn terraform

--- main.tf
+++ main.tf
@@ -10,13 +10,6 @@ resource "aws_security_group" "sg" {
     cidr_blocks = var.sg_subnet_cidr
   }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
 
   egress {
     from_port   = 0
