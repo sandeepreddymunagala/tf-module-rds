9593a8b68a592b7f85e0919332562cb90246b2bc
sandeep <munagalasandeepreddy@gamail.com>
2024-05-15 10:08:32 +0200

    learn terraform

--- main.tf
+++ main.tf
@@ -1,9 +1,15 @@
-resource "aws_rds_cluster" "default" {
-  cluster_identifier      = "${var.component}-${var.env}"
-  engine                  = "aurora-mysql"
-  engine_version          = "5.7.mysql_aurora.2.03.2"
-  database_name           = "mydb"
-  master_username         = "foo"
-  master_password         = "bar"
+resource "aws_db_subnet_group" "main" {
+  name       = "${var.component}-${var.env}"
+  subnet_ids = var.subnet_ids
+  tags       = merge({ Name = "${var.component}-${var.env}" }, var.tags)
+}
+
+resource "aws_rds_cluster" "main" {
+  cluster_identifier     = "${var.component}-${var.env}"
+  engine                 = var.engine
+  engine_version         = var.engine_version
+  database_name          = var.db_name
+  master_username         = data.aws_ssm_parameter.username
+  master_password         = data.aws_ssm_parameter.password
   backup_retention_period = 5
 }
--- variables.tf
+++ variables.tf
@@ -3,3 +3,5 @@ variable "component" {}
 variable "engine" {}
 variable "engine_version" {}
 variable "db_name" {}
+variable "subnet_ids" {}
+variable "tags" {}
