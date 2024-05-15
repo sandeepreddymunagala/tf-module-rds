eb3b920322691bd42cea6047418bee7178bd8c62
sandeep <munagalasandeepreddy@gamail.com>
2024-05-15 17:48:05 +0200

    learn terraform

--- main.tf
+++ main.tf
@@ -1,3 +1,35 @@
+resource "aws_security_group" "sg" {
+  name        = "${var.component}-${var.env}-sg"
+  description = "${var.component}-${var.env}-sg"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = var.sg_subnet_cidr
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.component}-${var.env}-sg"
+  }
+}
+
 resource "aws_db_subnet_group" "main" {
   name       = "${var.component}-${var.env}"
   subnet_ids = var.subnet_ids
--- variables.tf
+++ variables.tf
@@ -9,3 +9,5 @@ variable "kms_key_id" {}
 variable "kms_key_arn" {}
 variable "instance_count" {}
 variable "instance_class" {}
+variable "vpc_id" {}
+variable "sg_subnet_cidr" {}
