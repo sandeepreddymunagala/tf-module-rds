72c5fcd4783be8c4a148d0f5e6f213c34b06f6b8
sandeep <munagalasandeepreddy@gamail.com>
2024-05-15 11:27:30 +0200

    learn terraform

--- main.tf
+++ main.tf
@@ -11,6 +11,9 @@ resource "aws_rds_cluster" "main" {
   database_name          = var.db_name
   master_username         = data.aws_ssm_parameter.username.value
   master_password         = data.aws_ssm_parameter.password.value
+  db_subnet_group_name = aws_db_subnet_group.main.name
+  storage_encrypted      = true
+kms_key_id             = var.kms_key_arn
   backup_retention_period = 5
   skip_final_snapshot = true
 }
